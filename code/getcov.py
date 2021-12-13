deca9187270466a17d75aa61d1cc262b88618d1a
agroce <agroce@gmail.com>
2021-12-13 12:46:24 -0700

    actual code

--- code/getcov.py
+++ code/getcov.py
@@ -12,16 +12,5 @@ for d in g:
 
 for f in dirs:
     with open("out", 'w') as outf:
-        r = subprocess.call(["ulimit -t 1; ./fuzzgoat_ASAN " + f], shell=True, stderr=outf,stdout=outf)
-    with open("out", "rb") as outf:
-        sig = ""
-        for line in outf:
-            line = str(line, encoding="utf-8", errors='ignore')
-            if "fuzzgoat" in line:
-                sig += line.split(" in ")[1]
-        if (sig not in sigs) and (sig != ""):
-            print("NEW SIG:")
-            print(sig)
-            print("="*80)
-            sigs.append(sig)
-print(len(sigs))
+        r = subprocess.call(["ulimit -t 1; ./fuzzgoat_cov " + f], shell=True, stderr=outf,stdout=outf)
+
